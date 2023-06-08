9cacc6ba70647e56e04d35f79b2df27321a7a896
Rodrigodx <rodrigodx52@gmail.com>
2023-06-08 13:23:42 -0300

    Adicionado atributo 'clientName' e método 'consultarSaldo'

--- Conta.java
+++ Conta.java
@@ -1,8 +1,15 @@
 public class Conta {
 
     double saldoConta;
+	String name;
+	
     void sacar(double valor) {
         this.saldoConta -= valor;
     }
+
+	Double consultarSaldo(){
+		return saldoConta;
+	}
+
 
 }
