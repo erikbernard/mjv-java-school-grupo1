b88ab78ffaafe6a88a4400bb92e4590a3cd918a5
alvaro <alvarobrzezinski41@gmail.com>
2023-06-08 19:42:56 -0300

    adicionando atributos dataNascimento e extrato, bem como método extrato

--- Conta.java
+++ Conta.java
@@ -2,6 +2,8 @@ public class Conta {
 
     double saldoConta;
     String nomeCliente;
+    String dataNascimento;
+    float extrato;
 	
     void sacar(double valor) {
         this.saldoConta -= valor;
@@ -9,6 +11,10 @@ public class Conta {
 
     Double consultarSaldo(){
 	return saldoConta;
+    }
+
+    void extrato () {
+        System.out.println (this.nomeCliente", o extrato da sua conta é de: " this.extrato)
     }
 
 
